328740e2a03478108b5160ffa73c8e6008183e2b
Sean Chalmers <sclhiannan@gmail.com>
2019-01-22 21:57:49 +1000

    Added some more commands, worse than pringles

--- test/Ed/File.hs
+++ test/Ed/File.hs
@@ -6,6 +6,9 @@ import           Prelude           hiding (FilePath)
 
 import           Control.Lens      (snoc)
 
+import           Data.Bifoldable   (bifold)
+import           Data.Bifunctor    (first, second)
+
 import qualified Data.Text         as T
 import qualified Data.Text.IO      as T
 
@@ -14,23 +17,69 @@ import qualified Hedgehog.Gen      as Gen
 import qualified Hedgehog.Range    as Range
 
 import           Ed.Types          (Buffer (..), Cmd_Append (..),
+                                    Cmd_AppendAt (..), Cmd_DeleteLine (..),
                                     Cmd_PrintAll (..))
 
 import           Turtle
 
 import           System.Posix.Temp (mkstemp)
 
-
 resetEdFile :: FilePath -> IO ()
 resetEdFile edFile = sh $
-  testfile edFile >>= (`when` (rm edFile)) >> touch edFile
+  testfile edFile
+  >>= (`when` (rm edFile))
+  >> touch edFile
 
-edCmdsOnFile :: [Text] -> FilePath -> IO ()
-edCmdsOnFile cmds edFile = sh $
-  procs "ed" ["-s", format fp edFile] . select . textToLines $ T.unlines cmds
+edCmdsOnFile :: FilePath -> [Text] -> IO ()
+edCmdsOnFile edFile = sh .
+  procs "ed" ["-s", format fp edFile]
+  . select
+  . textToLines
+  . T.unlines
 
 readEdFile :: FilePath -> IO Text
 readEdFile edFile = T.readFile (T.unpack $ format fp edFile)
+
+genLineNum :: MonadGen m => Buffer v -> m Word
+genLineNum (Buffer b) = Gen.word (Range.linear 1 (fromIntegral $ length b))
+
+genTextInp :: MonadGen m => m Text
+genTextInp = Gen.text (Range.linear 1 100) Gen.alphaNum
+
+cAppendAt
+  :: ( MonadGen n
+     , MonadIO m
+     , MonadTest m
+     )
+  => FilePath
+  -> Command n m Buffer
+cAppendAt edFile =
+  let
+    gen b = Just $
+      Cmd_AppendAt <$> genLineNum b <*> genTextInp
+
+    execute (Cmd_AppendAt ln i) = evalIO $
+      edCmdsOnFile edFile
+        [ format (d%"a") ln
+        , i
+        , "."
+        , "w"
+        , "q"
+        ]
+      >> readEdFile edFile
+  in
+    Command gen execute
+    [ Require $ \(Buffer b) (Cmd_AppendAt ln _) ->
+        ln >= 0 && ln < fromIntegral (length b) && not (null b)
+
+    , Update $ \(Buffer b) (Cmd_AppendAt ln i) _ ->
+        Buffer . bifold . second (i:) . splitAt (fromIntegral ln) $ b
+
+    , Ensure $ \(Buffer _old) (Buffer new) (Cmd_AppendAt ln i) out -> do
+        T.unlines new === out
+        -- Can't get here without having perfomed a valid insert, so totes safe, right?...RIGHT!?
+        new !! (fromIntegral ln) === i
+    ]
 
 cAppendText
   :: ( MonadGen n
@@ -41,11 +90,17 @@ cAppendText
   -> Command n m Buffer
 cAppendText edFile =
   let
-    gen _ = Just $ Cmd_Append <$> (Gen.text (Range.linear 1 100) Gen.alphaNum)
+    gen _ = Just $ Cmd_Append <$> genTextInp
 
-    execute (Cmd_Append t) = evalIO $ do
-      edCmdsOnFile ["a", t, ".", "w", "q"] edFile
-      readEdFile edFile
+    execute (Cmd_Append t) = evalIO $
+      edCmdsOnFile edFile
+        [ "a"
+        , t
+        , "."
+        , "w"
+        , "q"
+        ]
+      >> readEdFile edFile
 
   in
     Command gen execute
@@ -64,14 +119,40 @@ cPrintAll edFile =
   let
     gen _ = Just $ Gen.constant Cmd_PrintAll
 
-    execute _ = evalIO $
-      readEdFile edFile
+    execute _ = evalIO $ readEdFile edFile
 
   in
     Command gen execute
-    [ Require $ \(Buffer cur) _ -> not $ null cur
+    [ Require $ \(Buffer cur) _                  -> not $ null cur
     , Ensure  $ \(Buffer old) (Buffer new) _ _   -> old === new
     , Ensure  $ \_            (Buffer new) _ out -> out === T.unlines new
+    ]
+
+cDeleteLine
+  :: ( MonadIO m
+     , MonadTest m
+     , MonadGen n
+     )
+  => FilePath
+  -> Command n m Buffer
+cDeleteLine edFile =
+  let
+    gen (Buffer b) = Just $
+      Cmd_DeleteLine <$> Gen.word (Range.linear 1 (fromIntegral $ length b))
+
+    execute (Cmd_DeleteLine n) = evalIO $ do
+      edCmdsOnFile edFile [format (d%"d") n, "w", "q"]
+      readEdFile edFile
+  in
+    Command gen execute
+    [ Require $ \(Buffer b) (Cmd_DeleteLine n) ->
+        n >= 0 && n < fromIntegral (length b) && not (null b)
+
+    , Update $ \(Buffer b) (Cmd_DeleteLine n) _ ->
+        Buffer
+        . bifold
+        . first (take (fromIntegral n - 1))
+        $ splitAt (fromIntegral n) b
     ]
 
 prop_ed_blackbox_file :: Property
@@ -81,7 +162,7 @@ prop_ed_blackbox_file = property $ do
 
   let
     -- Prepare our list of possible commands
-    cmds = ($ edFile) <$> [cAppendText, cPrintAll]
+    cmds = ($ edFile) <$> [cAppendText, cPrintAll, cDeleteLine, cAppendAt]
     -- Initialise our 'state'
     initialState = Buffer mempty
 
@@ -94,5 +175,6 @@ prop_ed_blackbox_file = property $ do
   -- Run the tests!
   executeSequential initialState actions
 
-  -- Clean up our temp files
+  -- Clean up our temp file
+  -- This could be configurable so that on a failing test, we don't delete the file ?
   evalIO . sh $ rm edFile
--- test/Ed/Types.hs
+++ test/Ed/Types.hs
@@ -8,9 +8,22 @@ import qualified System.Process as P
 
 import           Data.Text      (Text)
 
+data EdProc = EdProc
+  { _edIn  :: Handle
+  , _edOut :: Handle
+  , _edPh  :: P.ProcessHandle
+  }
+
 newtype Buffer (v :: * -> *) =
   Buffer [Text]
   deriving (Show, Eq)
+
+data Cmd_AppendAt (v :: * -> *) =
+  Cmd_AppendAt Word Text
+  deriving (Show, Eq)
+
+instance HTraversable Cmd_AppendAt where
+  htraverse _ (Cmd_AppendAt n t) = pure (Cmd_AppendAt n t)
 
 data Cmd_PrintAll (v :: * -> *) =
   Cmd_PrintAll
@@ -26,8 +39,9 @@ newtype Cmd_Append (v :: * -> *) =
 instance HTraversable Cmd_Append where
   htraverse _ (Cmd_Append t) = pure (Cmd_Append t)
 
-data EdProc = EdProc
-  { _edIn  :: Handle
-  , _edOut :: Handle
-  , _edPh  :: P.ProcessHandle
-  }
+newtype Cmd_DeleteLine (v :: * -> *) =
+  Cmd_DeleteLine Word
+  deriving (Show, Eq)
+
+instance HTraversable Cmd_DeleteLine where
+  htraverse _ (Cmd_DeleteLine t) = pure (Cmd_DeleteLine t)
