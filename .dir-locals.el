6c0330b1864dd2ef0621fd265e41b375884bd8ce
Jack Kelly <jack.kelly@data61.csiro.au>
2019-01-31 11:45:51 +1100

    Set up level02 and level03
    Turn solved level01 into starting level02
    Turn solved level02 into starting level03

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,7 @@
 (("coffee-state" . ((nil . ((dante-target . "coffee-state")))))
  ("level01" . ((nil . ((dante-target . "level01")))))
+ ("level02" . ((nil . ((dante-target . "level02")))))
+ ("level03" . ((nil . ((dante-target . "level03")))))
  (nil . ((dante-repl-command-line
           . ("nix-shell" "--run"
              (concat "cabal new-repl " dante-target " --builddir=dist/dante"))))))
