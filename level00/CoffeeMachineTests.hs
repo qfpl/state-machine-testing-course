bdd6c12eaf4afd584f3488c15d2d598152e486f0
Sean Chalmers <sclhiannan@gmail.com>
2019-05-23 14:12:49 +1000

    some updates based on feedback

--- level00/CoffeeMachineTests.hs
+++ level00/CoffeeMachineTests.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TemplateHaskell #-}
 module CoffeeMachineTests (propertyTests) where
 
-import Control.Lens (Prism', prism, matching, preview, review)
+import Control.Applicative (liftA2)
+import Control.Lens (Prism', matching, preview, review)
+import Control.Lens.TH (makePrisms)
 
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Hedgehog (testProperty)
@@ -10,7 +13,7 @@ import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
--- Starting
+
 addTen :: Int -> Int
 addTen = (+10)
 
@@ -19,6 +22,12 @@ prop_addTen = property $ do
   n <- forAll $ Gen.int (Range.linear 0 1000)
   addTen n === n + 10
 
+
+
+
+
+-- This function is broken, run the tests and see the failure output, then use
+-- that information to fix this function.
 badReverse :: [a] -> [a]
 badReverse [] = []
 badReverse (_:xs) = reverse xs
@@ -26,23 +35,73 @@ badReverse (_:xs) = reverse xs
 prop_badReverse :: Property
 prop_badReverse = property $ do
   xs <- forAll (Gen.list (Range.linear 0 1000) Gen.bool)
+  -- (optional): Are there other properties for reversing a list that could make
+  -- this test more robust?
   badReverse (badReverse xs) === xs
--- End Starting
 
--- Need a middle ground exercise
 
--- Recursive structures (??)
+
+
+
+
+-- We will start with the following data structure.
+newtype Coin = Coin Int deriving (Eq, Show)
+
+-- That has a pre-determined maximum value.
+maxCoinValue :: Int
+maxCoinValue = 1000000
+
+-- We're able to validate whether the Coin is valid.
+validCoin :: Coin -> Bool
+validCoin (Coin c) = c >= 0 && c <= maxCoinValue
+
+-- This is the function we're going to write some tests for.
+addCoins :: Coin -> Coin -> Maybe Coin
+addCoins (Coin a) (Coin b) = 
+  if a + b <= maxCoinValue
+    then Just (Coin $ a + b)
+    else Nothing
+
+-- Write our generator for Coin
+genCoin :: MonadGen m => m Coin
+genCoin = Coin <$> Gen.int (Range.linear 0 maxCoinValue)
+
+-- Test our 'normal' case, aka the happy path
+prop_addCoins_Normal :: Property
+prop_addCoins_Normal = property $ do
+  Coin a <- forAll genCoin
+  b <- forAll $ Gen.element [0, maxCoinValue - a]
+  (Coin a) `addCoins` (Coin b) === Just (Coin $ a + b)
+
+-- Test the 'overflow' case, aka the sad path
+prop_addCoins_Overflow :: Property
+prop_addCoins_Overflow = property $ do
+  Coin a <- forAll genCoin
+  b <- forAll $ Gen.element [maxCoinValue - a + 1, maxCoinValue]
+  (Coin a) `addCoins` (Coin b) === Nothing
+
+-- Instead of having separate properties, we can combine them into a single property test.
+prop_addCoins :: Property
+prop_addCoins = property $ do
+  ( ca@(Coin a), cb@(Coin b) ) <- forAll $ liftA2 (,) genCoin genCoin
+
+  let c = Coin (a + b)
+
+  addCoins ca cb === if validCoin c 
+    then Just c 
+    else Nothing
+
+
+
+
+
+
+-- Recursive structures
 data MyTree a
   = Leaf a
   | Node (MyTree a) (MyTree a)
   deriving (Show, Eq)
-
--- Provide prisms, it's not a lens course
-_Node :: Prism' (MyTree a) (MyTree a, MyTree a)
-_Node = prism (uncurry Node) (\x -> case x of Node a b -> Right (a,b); _ -> Left x)
-
-_Leaf :: Prism' (MyTree a) a
-_Leaf = prism Leaf (\x -> case x of Leaf a -> Right a; _ -> Left x)
+$(makePrisms ''MyTree)
 
 -- Fun generator to write, requires documentation diving (too much?)
 genMyTree :: Gen a -> Gen (MyTree a)
@@ -81,6 +140,9 @@ thirdPrismLaw genS l = property $ forAll genS >>= \s ->
     Left t -> matching l t === Left s
     Right _ -> success  -- Not a matching value, disregard
 
+
+-- Helper function, I don't think I would expect participants to write this. But
+-- I probably wouldn't include it as it's just where my head went.
 prop_myTreePrisms :: TestTree
 prop_myTreePrisms = testGroup "MyTree prisms satisfy prism laws"
   [ mkLawProps "_Node" gTree ((,) <$> gTree <*> gTree) _Node
@@ -95,11 +157,18 @@ prop_myTreePrisms = testGroup "MyTree prisms satisfy prism laws"
       , testProperty "Second law" $ secondPrismLaw genS p
       , testProperty "Third law" $ thirdPrismLaw genS p
       ]
--- End slightly silly
+
+
+
+
+
 
 propertyTests :: TestTree
 propertyTests = testGroup "Level00 - Property Tests"
   [ testProperty "Addition still works" prop_addTen
   , testProperty "Bad reverse is bad" prop_badReverse
   , prop_myTreePrisms
+  , testProperty "Add Coins" prop_addCoins
+  , testProperty "Add Coins (Normal)" prop_addCoins_Normal
+  , testProperty "Add Coins (Overflow)" prop_addCoins_Overflow
   ]
--- level00/README.md
+++ level00/README.md
@@ -1,18 +1,18 @@
 # Level00
 
-This level is intened to serve as a terse introduction to 'property-based testing'. 
+This level is a terse introduction to 'property-based testing'. 
 
 A significant feature of property-based testing is the randomised generation of
 inputs for a given test. 
 
-As an example, if we wanted to test a function:
+Consider this function:
 
 ```haskell
 addTen :: Int -> Int
 ```
 
-Using every `Int` between 0 and 1000, inclusive. Checking that the result is
-equal to the input, plus 10:
+If our spec dictates the output should be equal to the input plus ten, over the
+range of 0 to 1000 inclusive. We can express that with a property test:
 
 ```haskell
 prop_myFunc :: Property
@@ -21,10 +21,10 @@ prop_myFunc = property $ do
   myFunc x === (x + 10)
 ```
 
-This will generate 100 test cases (Hedgehog default) and check that your
-post-condition holds in all cases. A failing example will trigger the shrinking
-process.
+By default, Hedgehog tests each property with 100 random inputs. A failing
+example will trigger its shrinking process and try to find a minimal
+counterexample to the property.
 
-# Task
+# Your Task
 
-Complete the tests by completing the various generators and pre/post-conditions.
+Complete the tests by filling in the various generators and pre/post-conditions.
